8caa08e7700d33abc3550484f659ab1c8476cae0
Artin Mohammadi <sheikhartin@gmail.com>
2022-07-05 21:03:31 +0430

    The game in C++

---
+++ fizzbuzz.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  int rng_start;
+  int rng_end;
+  std::cout << "Enter the start and end of the range: ";
+  std::cin >> rng_start >> rng_end;
+  if (rng_start >= rng_end) {
+    std::cout << "The start must be less than the end." << std::endl;
+    return 1;
+  }
+
+  for (int i = rng_start; i <= rng_end; i++) {
+    if (i % 15 == 0) {
+      std::cout << "FizzBuzz" << std::endl;
+    } else if (i % 3 == 0) {
+      std::cout << "Fizz" << std::endl;
+    } else if (i % 5 == 0) {
+      std::cout << "Buzz" << std::endl;
+    } else {
+      std::cout << i << std::endl;
+    }
+  }
+
+  return 0;
+}
